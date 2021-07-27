531c45f83d7eae195e6d6f61f9a65c852128d65d
BabintsevaS <sb.babintseva@gmail.com>
2021-07-27 10:08:23 -0700

    Test and yml files were changed

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -3,10 +3,13 @@ package ru.netology.web;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
 
@@ -16,22 +19,21 @@ import static com.codeborne.selenide.Selenide.*;
 public class RegistrationTest {
     @Test
     void shouldToBookCard() {
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.DATE, 3);
-        String outputDate = dateFormat.format(c.getTime());
+        LocalDate meetingDate = LocalDate.now().plusDays(3);;
+        String outputDate = meetingDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Челябинск");
         $$(".menu-item__control").last().click();
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
         form.$("[data-test-id=date] input").setValue(outputDate);
         form.$("[data-test-id=name] input").setValue("Иванов Иван");
         form.$("[data-test-id=phone] input").setValue("+71234567891");
         form.$(".checkbox__box").click();
         form.$(".button__text").click();
         $(withText("Успешно!")).should(Condition.visible, Duration.ofSeconds(15));
+        $(withText(outputDate)).should(Condition.visible);
     }
 
 }
